c19502f292e347992a95adc59888086b56eca2f2
aaronespinoza <aarone@berkeley.edu>
2022-01-17 00:04:38 -0800

    getting no directory found

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const htmlRoute = require("./route/htmlroute");
-const apiRoute = require("./route/apiroute");
+const htmlRoute = require("./routes/htmlRoute");
+const apiRoute = require("./routes/apiRoute");
 
 app.use(
   express.urlencoded({
